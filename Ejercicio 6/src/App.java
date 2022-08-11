31e5d4ea0e07141482d5256f5df43cc307162ab5
Leandro Sánchez <leanmsan@gmail.com>
2022-08-11 20:59:07 -0300

    Corregido

--- Ejercicio 6/src/App.java
+++ Ejercicio 6/src/App.java
@@ -4,6 +4,10 @@ public class App {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese una palabra");
         String s1 = sc.nextLine();
-        
+        String s2 = s1;
+        for (int i = s1.length() - 2; i >= 0; i--) {
+            s2 = s2 + s2.charAt(i);
+        }
+        System.out.println(s2);
     }
 }
