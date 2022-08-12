7dbd15cb122c5393f68a078b1efc58f0eabcf404
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2022-08-11 21:15:01 -0300

    Delete Ejercicio 8/src directory

--- Ejercicio 8/src/App.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese una frase");
-        String s1 = sc.nextLine();
-    }
-}
