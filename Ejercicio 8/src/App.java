f031e2aeda9d26e4d71987b1b388e88f75736549
Leandro Sánchez <leanmsan@gmail.com>
2022-08-11 21:31:52 -0300

    Terminar

--- Ejercicio 8/src/App.java
+++ Ejercicio 8/src/App.java
@@ -4,6 +4,6 @@ public class App {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese una frase");
         String s1 = sc.nextLine();
-        
+        int espacio = s1.indexOf(" ");
     }
 }
