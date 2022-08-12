9607c59040ac3f5fec648f201c5ce35680d6d962
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2022-08-11 21:31:26 -0300

    Add files via upload

---
+++ Ejercicio 8/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una frase");
+        String s1 = sc.nextLine();
+        int espacio = s1.indexOf(" ");
+    }
+}
