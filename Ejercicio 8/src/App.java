5d611d887835962767f6ef90e3d41a098e40236f
Leandro Sánchez <leanmsan@gmail.com>
2022-08-11 21:10:50 -0300

    Para terminar

---
+++ Ejercicio 8/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una frase");
+        String s1 = sc.nextLine();
+    }
+}
