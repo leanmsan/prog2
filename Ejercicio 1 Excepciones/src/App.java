4b020ef122329c3cf972d5d20158ddd93b7fc7aa
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2022-08-18 11:56:37 -0300

    Terminado

--- Ejercicio 1 Excepciones/src/App.java
+++ Ejercicio 1 Excepciones/src/App.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Dime un numero");
-            String s1 = sc.nextLine();
-            int numero = Integer.parseInt(s1);
-            int res = numero * 2;
-            System.out.println("El resultado es: " + res);
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.println("Dime un numero");
+                String s1 = sc.nextLine();
+                int numero = Integer.parseInt(s1);
+                int res = numero * 2;
+                System.out.println("El resultado es: " + res);
+            }
         }
 
         catch(Exception ex) {
@@ -17,6 +18,7 @@ public class App {
 
         finally {
             System.out.println("Yo siempre me ejecuto xd");
+            System.out.println("Mi turnoooo putitoooos");
         }
     }
 }
