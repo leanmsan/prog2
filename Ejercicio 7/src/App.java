d033a8f838a22c2481a49ce5d845180086a51f2d
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2022-08-18 08:43:21 -0300

    Terminado

---
+++ Ejercicio 7/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime algo");
+        String s1 = sc.nextLine();
+        System.out.println("Dime la palabra que quieres pasar a mayuscula");
+        String s2 = sc.nextLine();
+        System.out.println(s1.replace(s2, s2.toUpperCase()));
+    }
+}
