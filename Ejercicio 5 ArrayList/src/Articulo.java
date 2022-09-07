408ad0ed920c5cf5a1e825431e4e4970b6ee6a13
Leandro Sánchez <leanmsan@gmail.com>
2022-09-07 18:16:31 -0300

    Para Terminar

--- Ejercicio 5 ArrayList/src/Articulo.java
+++ Ejercicio 5 ArrayList/src/Articulo.java
@@ -17,4 +17,56 @@ public class Articulo {
         Precio = 0;
         Stock_Actual = 0;
     }
+
+    public int getCod_Barras() {
+        return Cod_Barras;
+    }
+
+    public void setCod_Barras(int cod_Barras) {
+        Cod_Barras = cod_Barras;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double precio) {
+        Precio = precio;
+    }
+
+    public int getStock_Actual() {
+        return Stock_Actual;
+    }
+
+    public void setStock_Actual(int stock_Actual) {
+        Stock_Actual = stock_Actual;
+    }
+
+    public String datosArticulos() {
+        return "Codigo de Barras: " + Cod_Barras + ". Nombre: " + Nombre + ". Precio: " + Precio + ". Stock Actual: "
+                + Stock_Actual;
+    }
+
+    public int Compra(int cantidad) {
+        Stock_Actual = Stock_Actual + cantidad;
+        return Stock_Actual;
+    }    
+
+    public double Venta(int cantidad) {
+        if (Stock_Actual >= cantidad) {
+            double precio_final = Precio * cantidad;
+            Stock_Actual = Stock_Actual - cantidad;
+            return precio_final;
+        } else {
+            return -1;
+        }
+    }
 }
