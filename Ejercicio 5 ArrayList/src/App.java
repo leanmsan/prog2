23bc483c81bcece93bf65de043dfa031c36ed643
Leandro Sánchez <leanmsan@gmail.com>
2022-09-07 18:16:20 -0300

    Para Terminar

--- Ejercicio 5 ArrayList/src/App.java
+++ Ejercicio 5 ArrayList/src/App.java
@@ -1,7 +1,56 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+
+        //lista de rubros
+        ArrayList<Rubro> nuevoRubro = new ArrayList<>();
+
+        //menu
+        int opcion = 0;
+        do {
+            menu();
+            switch (opcion) {
+                case 1:
+                    altaRubro(sc, nuevoRubro);
+                    break;
+                case 2: 
+                    break;
+                case 3: 
+                    break;
+                case 4:
+                    break;
+                case 5:  
+                    break;
+                case 6: 
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9: 
+                    break;
+            }
+        } while (opcion != 10);
+    }
+
+    private static void menu() {
+        System.out.println("\n" + "Seleccione una opcion");
+        System.out.println("1- Dar de alta un rubro");
+        System.out.println("2- Dar de alta un articulo");
+        System.out.println("3- Modificar precio de un articulo");
+        System.out.println("4- Ver lista de rubros");
+        System.out.println("5- Ver los articulos de un rubro");
+        System.out.println("6- Comprar un articulo");
+        System.out.println("7- Vender un articulo");
+        System.out.println("8- Buscar un articulo");
+        System.out.println("9- Ver lista de articulos por rubro");
+        System.out.println("10- Salir");
+    }
+
+    private static void altaRubro(Scanner sc, ArrayList<Rubro> nuevoRubro) {
+
     }
 }
--- Ejercicio 5 ArrayList/src/Rubro.java
+++ Ejercicio 5 ArrayList/src/Rubro.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+
 public class Rubro {
     private int Cod_Rubro;
     private String Descripcion ;
-    
+    private ArrayList Articulos;
+
+    public Rubro(int cod_Rubro, String descripcion, ArrayList articulos) {
+        Cod_Rubro = cod_Rubro;
+        Descripcion = descripcion;
+        Articulos = articulos;
+    }
+
+    public Rubro() {
+        Cod_Rubro = 0;
+        Descripcion = "";
+    }
+
+    public int getCod_Rubro() {
+        return Cod_Rubro;
+    }
+
+    public void setCod_Rubro(int cod_Rubro) {
+        Cod_Rubro = cod_Rubro;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public String DatosRubro() {
+        return "Codigo de Rubro:" + Cod_Rubro + ". Descripcion: " + Descripcion;
+    }
+
+    public void altaArticulo() {
+        ArrayList<Articulo> articulos = new ArrayList<>();
+    }
 }
